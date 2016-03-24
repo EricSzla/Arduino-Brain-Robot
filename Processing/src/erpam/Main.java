cc46f8b5e36f94c3106b3eefcfb00072d985f8c0
Vopic <eryk1994@yahoo.ie>
2016-03-24 17:33:39 +0000

    Initial Code for processing

---
+++ Processing/src/erpam/Main.java
@@ -0,0 +1,17 @@
+package erpam;
+import processing.core.*;
+/**
+ * Created by Eryk Szlachetka and Pamela Sabio on 14/03/2016.
+ **/
+
+public class Main extends PApplet {
+
+
+    public static void main(String[] args)
+    {
+        PApplet.main(Main.class.getName());
+        /*String[] a = {"MAIN"};
+        PApplet.runSketch( a, new Main());*/
+    }
+}
+
