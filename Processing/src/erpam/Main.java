77c694ab4a61d934d4b32c3dfe7b282e1ead9e13
Vopic <eryk1994@yahoo.ie>
2016-03-25 21:28:50 +0000

    Added keyPressed, controls for BlueTooth

--- Processing/src/erpam/Main.java
+++ Processing/src/erpam/Main.java
@@ -1,6 +1,7 @@
 package erpam;
 import processing.core.*;
 import processing.core.PImage;
+import processing.video.*;
 import processing.serial.*;
 
 /**
@@ -11,9 +12,19 @@ public class Main extends PApplet {
 
     // For Arduino connection
     Serial myPort;
+    RemoteControl BT;
+
+    Movie trailer;
+    //boolean[] keys = new boolean[512];
+
     PImage erpam;
-	int x = 0;
-	
+    int x = 0;
+
+    int choice = 0;
+    float speed = 0;
+    float leftspeed = 0;
+    char currentGear = 'N';
+
     public void settings()
     {
         size(700, 500);
@@ -30,11 +41,36 @@ public class Main extends PApplet {
         frameRate(60);
         erpam = loadImage("erpam.png");
 
+        //trailer = new Movie(this,"trailer.mp4");
+        //trailer.loop()
+
+        BT = new RemoteControl(this);
+        smooth();
     }
 
     public void draw()
     {
+        switch(choice)
+        {
+            case 0:
+                menu();
+                break;
+            case 1:
+                BT.render(currentGear);
+                break;
+            case 2:
+                //mapping();
+                break;
+            default:
+                menu();
+        }
 
+    }
+
+    public void mouseClicked()
+    {
+        // To be completed
+        choice = 1;
     }
 
     public void menu()
@@ -51,6 +87,121 @@ public class Main extends PApplet {
         fill(255);
         text("---> TERRITORY MAPPING", x, height/3);
 
+    }
+
+    public void keyPressed() {
+
+        //float value = map(valueReturned, 90, 230, 60, 230);
+        //String passValue = "L" + value;
+        // keys[keyCode] = true; Slows down the reaction time ?
+        if(choice == 1) {
+            if (keyCode == 'W') {
+
+                if(speed < 0 || leftspeed < 0)
+                {
+                    speed = 0;
+                    leftspeed = 0;
+                    currentGear = 'N';
+
+                }else {
+                    speed = 200;
+                    leftspeed = 170;
+                    currentGear = 'D';
+                }
+
+            } else if (keyCode == 'S') {
+                if (speed == 0 && leftspeed == 0) {
+                    speed = -200;
+                    leftspeed = -190;
+                    currentGear = 'R';
+                } else {
+                    speed = 0;
+                    leftspeed = 0;
+                    currentGear = 'N';
+                }
+            } else if (keyCode == 'A') {
+                if(speed >= 0 && leftspeed >= 50) {
+                    speed = 200;
+                    leftspeed -= 50;
+                }else if(speed < 0 && leftspeed < 0)
+                {
+                    speed = -200;
+                    leftspeed += 50;
+                }
+            } else if (keyCode == 'D') {
+                if(speed >= 50 && leftspeed >= 0) {
+                    leftspeed = 170;
+                    speed -= 50;
+                }else if(speed < 0 && leftspeed < 0)
+                {
+                    leftspeed = -190;
+                    speed += 50;
+                }
+
+            } else {
+                System.out.print("im in else");
+                leftspeed = 0;
+                speed = 0;
+            }
+
+            myPort.write("R" + speed + ",");
+            myPort.write("L" + leftspeed + ",");
+
+            BT.update(speed,leftspeed);
+
+        }
+
+
+    }
+
+    public void motorSpeeds(){
+        /*
+            This method has to read in a value from Arduino and set the leftMotor and rightMotor slider value to the passed value e.g.
+            if(myPort.available() > 0)
+            {
+                if(strcmp(var,0,1) == 'L') // OR SOMETHING LIKE THAT TO BE CHECKED
+
+                if(var.read() >= 0)
+                {
+                    LeftMotor(var.read());
+                }
+r
+                if(var2.read() >=0)
+                {
+                    // HAS TO BE MAPPED AS ONE MOTOR IS FASTER THAN ANOTHER !
+                    // TO BE CHECKED IF ITS LEFT OR RIGHT !                    <<<------------------------
+                    float rightSpeed = map(var2.read(),0,200,0,255);
+                    RightMotor(rightSpeed);
+                }
+            }
+
+            Arduino should have a code like:
+            var.write(leftMotorCurrentSpeed);
+            var2.write(rightMotorCurrentSpeed);
+         */
+    }
+
+    public void Turning(float value)
+    {
+        /*
+            This method should set the turning slide depending on either:
+                - Keyboard input e.g.  input = A then decrease the slider,  input == D then increase the slider
+                - Difference between Left and Right motor's speed. Use map function?
+
+
+            if(keyPressed == 'D')
+            {
+                Turning(--value);
+            }else if(keyPressed == 'A')
+            {
+                Turning(--value);
+            }
+
+         */
+    }
+    public void movieEvent(Movie m)
+    {
+        m.read();
     }
 
     public static void main(String[] args)
