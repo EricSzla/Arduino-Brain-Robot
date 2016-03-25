f8b125145d66583d14ed2905a622b53ea43aeaae
Pamela Sabio <C14484542@mydit.ie>
2016-03-25 21:05:49 +0000

    Added render and update

--- Processing/src/erpam/Main.java
+++ Processing/src/erpam/Main.java
@@ -1,48 +1,62 @@
-package erpam;
+package ie.dit;
 import processing.core.*;
-import processing.serial.Serial;
-import processing.video.Movie;
+import controlP5.*;
 
 /**
- * Created by Eryk Szlachetka and Pamela Sabio on 14/03/2016.
- **/
+ * Created by Eryk Szlachetka and Pamela Sabio on 19/03/2016.
+ */
 
-public class Main extends PApplet {
+public class RemoteControl{
 
-    // For Arduino connection
-    Serial myPort;
-    PImage erpam;
-    int choice = 0;
+    // Declare Variables
+    PApplet papplet;
+    ControlP5 cp5;
+    Slider leftMotorSlider;
+    Slider rightMotorSlider;
+    Slider turningSlider;
 
+    // Constructor
+    RemoteControl(PApplet papplet) {
 
-    public void settings()
-    {
-        size(700, 500);
+        this.papplet = papplet;
+        this.cp5 = new ControlP5(this.papplet); // Iniliatilze new control p5
+
+        // Add motorSliders to cp5
+        this.leftMotorSlider = cp5.addSlider("LeftMotor");
+        this.rightMotorSlider = cp5.addSlider("RightMotor");
+        this.turningSlider = cp5.addSlider("Turning");
+
+        // Initialize the sliders
+        initialize();
     }
 
-    public void setup()
+
+    public void initialize()
     {
-        printArray(Serial.list()); // Prints the serial lists
-
-        // Start the Bluetooth port
-        String portName = Serial.list()[2]; //change the 0 to a 1 or 2 etc. to match the port for BT
-        myPort = new Serial(this, portName, 9600); // Initialize myPort and set the bit rate
-
-        frameRate(60);
-        erpam = loadImage("erpam.png");
-
-        smooth();
+        // Sets sliders position, size, range and hides it
+        this.leftMotorSlider.setPosition(papplet.width/10-20,papplet.height- (papplet.height/4)).setSize(20,100).setRange(0,255).hide();
+        this.rightMotorSlider.setPosition(papplet.width-papplet.width/10,papplet.height- (papplet.height/4)).setSize(20,100).setRange(0,255).hide();
+        this.turningSlider.setPosition(150,370).setWidth(400).setRange(255,0).setValue(128).setSliderMode(Slider.FLEXIBLE).setLabelVisible(false).hide();
     }
 
-    public void draw()
+    public void render(char gear)
     {
+        papplet.background(0);
+        leftMotorSlider.show();
+        rightMotorSlider.show();
+        turningSlider.show();
+        papplet.textSize(24);
+        papplet.textAlign(papplet.CENTER);
+        papplet.text("Gear: " + gear, papplet.width/2,papplet.height/2);
     }
 
-    public static void main(String[] args)
+    public void update(float speed, float leftspeed)
     {
-        PApplet.main(Main.class.getName());
-        /*String[] a = {"MAIN"};
-        PApplet.runSketch( a, new Main());*/
+        // Update controls
+        rightMotorSlider.setValue(papplet.map(speed,0,200,0,255));
+        leftMotorSlider.setValue(papplet.map(leftspeed,0,170,0,255));
+        //turningSlider.setValue();
+
     }
+
 }
-
