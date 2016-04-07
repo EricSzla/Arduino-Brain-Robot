824a94c2ea2bf4e5a123cdff5753e3c8b9299c80
Vopic <eryk1994@yahoo.ie>
2016-04-07 17:11:24 +0100

    Added headSet class

---
+++ Processing/src/erpam/HeadSet.java
@@ -0,0 +1,20 @@
+package erpam;
+import processing.core.PApplet;
+import processing.serial.Serial;
+
+/**
+ * Created by Eryk Szlachetka and Pamela Sabio on 02/04/2016.
+ */
+public class HeadSet {
+    PApplet papplet;
+
+    HeadSet(PApplet papplet) {
+
+        this.papplet = papplet;
+    }
+
+    public void pass(Serial myPort,String var)
+    {
+        myPort.write(var);
+    }
+}
